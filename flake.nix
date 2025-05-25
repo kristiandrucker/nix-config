52bb110dc9318ab4f73bd0b287bf57784cf33e0a
Kristian Drucker <kristian.drucker@gmail.com>
2025-05-25 17:05:16 +0200

    chore: bump nixpkgs stable to 25.05

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
   inputs = {
     # Nix ecosystem
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     systems.url = "github:nix-systems/default-linux";
     deploy-rs = {
       url = github:serokell/deploy-rs;
